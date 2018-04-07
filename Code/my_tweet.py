0cdddc089376d451daa9e2197d64c8c788b3ef5b
mio <9just.k.on@gmail.com>
2018-04-07 16:12:20 +0800

    try to get some twitter data

---
+++ Code/my_tweet.py
@@ -0,0 +1,5 @@
+consumer_key="smDn7PS3geCmoSKTxE8xSl5l9"
+consumer_secret="1jwuUZKWW43KnEAr22X9VQpGv8LjWfHQxPyiksPT2RzkP2RW22"
+
+access_token="2772189990-iSfSvQQyrL34d2me6xqNO3z4QF592lQfs5ETrWn"
+access_token_secret="9FagV662MQrdvwSTBo6SnfrMz5a1QOaV3OeJYhitGNyiq"
--- Code/tag_cloud.py
+++ Code/tag_cloud.py
@@ -3,25 +3,68 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud, STOPWORDS
+import re
+from sklearn.feature_extraction import stop_words
+import nltk
+
+def clean_str(string, TREC=False):
+  """
+    Tokenization/string cleaning for all datasets except for SST.
+    Every dataset is lower cased except for TREC
+  """
+  string = re.sub(r"[^A-Za-z0-9(),!?\'\`]", " ", string)
+  string = re.sub(r"\'s", " \'s", string)
+  string = re.sub(r"\'ve", " \'ve", string)
+  string = re.sub(r"n\'t", " n\'t", string)
+  string = re.sub(r"\'re", " \'re", string)
+  string = re.sub(r"\'d", " \'d", string)
+  string = re.sub(r"\'ll", " \'ll", string)
+  string = re.sub(r",", " , ", string)
+  string = re.sub(r"!", " ! ", string)
+  string = re.sub(r"\(", " \( ", string)
+  string = re.sub(r"\)", " \) ", string)
+  string = re.sub(r"\?", " \? ", string)
+  string = re.sub(r"\s{2,}", " ", string)
+  return string.strip() if TREC else string.strip().lower()
+
+def test(words):
+  word_dict = {}
+  result = ""
+  real_words = set(nltk.corpus.words.words())
+  for w in words:
+    if w not in stop_words.ENGLISH_STOP_WORDS and w in real_words and len(w)>1:
+      print(w)
+      result = result + w + " "
+  return result
+
+
+
 
 def drawname(kind):
   d = path.dirname(__file__)
 
   # Read text
   text = open(path.join(d, '../Data/Testdata/'+kind+'.in')).read()
+  text = clean_str(text)
+  # print(text)
+  words = text.split()
+
+# doc-term matrix with raw word count
+  final_text = test(words)
+  print(final_text)
 
   # read image
   mask = np.array(Image.open(path.join(d, "../Data/img/"+kind+".png")))
 
-  stopwords = set(STOPWORDS)
-  # stopwords.add("said")
+  # stopwords = set(STOPWORDS)
+  # stopwords.add("rt")
+  # # text = "aa bb cc dd aa cc aa dd aa cc aa cc"
 
-  wc = WordCloud(max_words=1000, mask=mask, stopwords=stopwords, margin=10,
-                 random_state=1).generate(text)
-
-  # generate word cloud
+  # # wc = WordCloud(max_words=1000, mask=mask, stopwords=stopwords, margin=10,
+  #                # random_state=1).generate(text)
+  wc = WordCloud(max_words=1000, mask=mask, margin=10,random_state=1).generate(final_text)
+  # # generate word cloud
   # wc.generate(text)
-
   # store to file
   wc.to_file(kind+".png")
 
