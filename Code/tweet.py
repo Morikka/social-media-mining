53162a489b8ff878e686c993a08a0adc5e643654
mio <9just.k.on@gmail.com>
2018-04-07 16:12:20 +0800

    try to get some twitter data

---
+++ Code/tweet.py
@@ -0,0 +1,20 @@
+import tweepy
+import my_tweet as mt
+
+auth = tweepy.OAuthHandler(mt.consumer_key,mt.consumer_secret)
+auth.set_access_token(mt.access_token,mt.access_token_secret)
+
+api = tweepy.API(auth)
+# for tweet in tweepy.Cursor(api.search,
+#                            q='cat',
+#                            count=100).items():
+#   print(tweet.created_at, tweet.text)
+
+with open('t.out','w') as f:
+  for tweet in tweepy.Cursor(api.search,
+                           q='cat',
+                           lang="en",
+                           count=100).items():
+    print(tweet.created_at, tweet.text)
+    f.write(tweet.text)
+    f.write("\n")
